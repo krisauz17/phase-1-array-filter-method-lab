6c4efc5a094349a0a739d225a093903ca41cc320
krishnendu basak <krisauz17@yahoo.com>
2022-10-27 22:35:36 +1100

    phase-1-array-filter-method-lab is done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, names){
+    return drivers.filter((match) => match.toLowerCase() === names.toLowerCase());
+}
+// const drivvers= [];
+function fuzzyMatch(drivers, letters){
+    return drivers.filter((match) => match.toUpperCase().indexOf(letters.toUpperCase())===0);
+}
+function matchName(drivers, nameMatch){
+    return drivers.filter((anotherMatch)=>anotherMatch.name ===nameMatch);
+}
